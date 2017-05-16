ca1015ea768661dd2f942868f81c7ad5dd685f39
Máximo J. García Martínez <onmax@users.noreply.github.com>
2017-05-16 12:06:42 +0200

    Funciona to' bien

--- practica_3/FSM_ascensor_grupo22.vhd
+++ practica_3/FSM_ascensor_grupo22.vhd
@@ -1,12 +1,11 @@
-
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 ENTITY FSM_ascensor IS 
 	PORT (clk, reset: IN STD_LOGIC; -- activo por flanco de subida
-	codigo_piso: IN STD_LOGIC_VECTOR (1 DOWNTO 0); --código binario del Nº de piso pulsado
- 	sube, baja: OUT STD_LOGIC; -- dirección movimiento del motor
- 	piso_donde_esta: OUT STD_LOGIC_VECTOR (1 DOWNTO 0)); -- código binario del piso donde se
- 	--encuentra que coincide con el código asignado a la luz del panel 
+	codigo_piso: IN STD_LOGIC_VECTOR (1 DOWNTO 0); --cÃ³digo binario del NÂº de piso pulsado
+ 	sube, baja: OUT STD_LOGIC; -- direcciÃ³n movimiento del motor
+ 	piso_donde_esta: OUT STD_LOGIC_VECTOR (1 DOWNTO 0)); -- cÃ³digo binario del piso donde se
+ 	--encuentra que coincide con el cÃ³digo asignado a la luz del panel 
 END FSM_ascensor;
 
 ARCHITECTURE FSM_arquitectura OF FSM_ascensor IS
@@ -16,8 +15,12 @@ BEGIN
 	PROCESS (e_actual, codigo_piso)
 	BEGIN
 		CASE e_actual IS
-			WHEN piso0 =>
-				IF codigo_piso = "01" THEN
+			WHEN piso0 =>		
+				IF codigo_piso = "00" THEN
+					e_futuro <= piso0;
+					sube <= '0';
+					baja <= '0';
+				ELSIF codigo_piso = "01" THEN
 					e_futuro <= piso1;
 					sube <= '1';
 					baja <= '0';
@@ -26,36 +29,38 @@ BEGIN
 					sube <= '1';
 					baja <= '0';
 				END IF;
+				piso_donde_esta <= "00";
 			WHEN piso1 =>
-				IF codigo_piso = "10" THEN
+				IF codigo_piso = "00" THEN
+					e_futuro <= piso0;
+					sube <= '0';
+					baja <= '1';
+				ELSIF codigo_piso = "01" THEN
+					e_futuro <= piso1;
+					sube <= '0';
+					baja <= '0';
+				ELSE
 					e_futuro <= piso2;
 					sube <= '1';
 					baja <= '0';
-				ELSE 
+				END IF;
+				piso_donde_esta <= "01";
+			WHEN piso2 =>
+				IF codigo_piso = "00" THEN
 					e_futuro <= piso0;
 					sube <= '0';
 					baja <= '1';
-				END IF;
-			WHEN piso2 =>
-				IF codigo_piso = "01" THEN
+				ELSIF codigo_piso = "01" THEN
 					e_futuro <= piso1;
 					sube <= '0';
 					baja <= '1';
-				ELSE 
-					e_futuro <= piso0;
+				ELSE
+					e_futuro <= piso2;
 					sube <= '0';
-					baja <= '1';
+					baja <= '0';
 				END IF;
+				piso_donde_esta <= "10";
 		END CASE;
-	END PROCESS;
-
-	PROCESS (e_actual, codigo_piso)
-	BEGIN 
-		IF(e_actual = piso2 AND codigo_piso = "11") THEN
-			piso_donde_esta<="11";
-		ELSE
-			piso_donde_esta<="00";
-		END IF;
 	END PROCESS;
 	
 	PROCESS
