6190ed47a97cad5470ae9b271b0f46ef1b34352b
Máximo J. García Martínez <onmax@users.noreply.github.com>
2017-05-22 17:50:07 +0200

    Update ascensor_completo_grupo22.vhd

--- practica_3/ascensor_completo_grupo22.vhd
+++ practica_3/ascensor_completo_grupo22.vhd
@@ -1,22 +1,31 @@
 LIBRARY ieee;
-USE ieee.STD_LOGIC_1164.all;
-
-ENTITY ascensor_completo IS
-	PORT(
-	piso_donde_va: IN STD_LOGIC_VECTOR (1 DOWNTO 0); 
-	codigo_piso: OUT STD_LOGIC_VECTOR (1 DOWNTO 0));
+USE ieee.std_logic_1164.ALL; 
+ENTITY ascensor_completo Is
+PORT(B:IN std_logic_vector(2 DOWNTO 0);clk,reset:IN std_logic; 
+sube,baja:OUT std_logic;piso_donde_esta:OUT std_logic_vector(1 DOWNTO 0));
 END ascensor_completo;
 
 ARCHITECTURE arquitectura_ascensor_completo OF ascensor_completo IS
+
+
+COMPONENT codifica_boton IS
+     PORT(piso_donde_va: IN STD_LOGIC_VECTOR (2 DOWNTO 0); codigo_piso: OUT STD_LOGIC_VECTOR (1 DOWNTO 0));
+END COMPONENT;
+
+COMPONENT FSM_ascensor IS 
+	PORT (clk, reset: IN STD_LOGIC;
+	codigo_piso: IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+ 	sube, baja: OUT STD_LOGIC;
+ 	piso_donde_esta: OUT STD_LOGIC_VECTOR(1 DOWNTO 0)); 
+END COMPONENT;	
+
+
+FOR U0:codifica_boton USE ENTITY work.codifica_boton(arquitectura_cod_boton);
+FOR U1:FSM_ascensor USE ENTITY work.FSM_ascensor(FSM_arquitectura);
+SIGNAL c:std_logic_vector(1 DOWNTO 0);
+
+
 BEGIN
- 	PROCESS(piso_donde_va) -- IMPORTANTE: debemos incluir aquí todas las entradas que luego intervengan en el resto del código
-                        -- asociado al bloque process 
-             BEGIN
-             CASE(piso_donde_va) IS 
-               	WHEN "01" => codigo_piso <= "00";          
-              	WHEN "10" => codigo_piso <= "01";          
-              	WHEN "00" => codigo_piso <= "10";                   
-              	WHEN OTHERS => codigo_piso <= "11";         -- Este caso no aparece en la tabla de verdad         
-              END CASE;
-         END PROCESS;
+U0:codifica_boton PORT MAP(piso_donde_va=>B,codigo_piso=>c);
+U1:FSM_ascensor PORT MAP(clk=>clk,reset=>reset,codigo_piso=>c,sube=>sube,baja=>baja,piso_donde_esta=>piso_donde_esta);
 END arquitectura_ascensor_completo;
