862ee6fc313512335bcd5e52277501ee07e6c7d2
Máximo J. García Martínez <onmax@users.noreply.github.com>
2017-05-23 11:29:36 +0200

    Update ascensor_completo_test_tb.vhd

--- practica_3/ascensor_completo_test_tb.vhd
+++ practica_3/ascensor_completo_test_tb.vhd
@@ -1,16 +1,34 @@
+---------------------------------------------------------------------------
+-- Este diseño es propietario el autor y puede ser utilizado con fines de
+-- estudio
+---------------------------------------------------------------------------
+-- Proyecto: Tercera práctica de Sistemas Digitales
+-- Diseño: Máquina de Estados Finitos
+-- Nombre del fichero: NOMBRE DEL FICHERO.vhd
+-- Autor: Raúl Carbajosa González (y160311) y Máximo García Martínez (y160242)
+-- Fecha: 23/05/2017
+-- Versión: 1.0
+-- Resumen: Contiene una entidad y una arquitectura de una
+-- máquina de estados finitos que controla el funcionamiento de un ascensor
+-- Se utilizan el tipo de datos STD_LOGIC para todas las señales.
+-- La arquitectura se realiza en estilo ESTRUCTURAL.
+-- Modificaciones:
+--
+-- Fecha Autor Versión Descripción del cambio
+-- ========================================================================
+--
+-- ========================================================================
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
 ENTITY ascensor_completo_test IS
-
-
 END ascensor_completo_test;
 
 
 ARCHITECTURE  ascensor_completo_arquitectura_test OF ascensor_completo_test IS
-COMPONENT codifica_boton IS
-     PORT(piso_donde_va: IN STD_LOGIC_VECTOR (2 DOWNTO 0); codigo_piso: OUT STD_LOGIC_VECTOR (1 DOWNTO 0));
-END COMPONENT;
+	COMPONENT codifica_boton IS
+	     PORT(piso_donde_va: IN STD_LOGIC_VECTOR (2 DOWNTO 0); codigo_piso: OUT STD_LOGIC_VECTOR (1 DOWNTO 0));
+	END COMPONENT;
 
 COMPONENT FSM_ascensor IS 
 	PORT (clk, reset: IN STD_LOGIC;
@@ -33,9 +51,10 @@ FOR U1:FSM_ascensor USE ENTITY work.FSM_ascensor(FSM_arquitectura);
      SIGNAL piso_donde_esta:std_logic_vector(1 DOWNTO 0);
      SIGNAL piso_donde_va: std_logic_vector(2 DOWNTO 0);
      SIGNAL codigo_piso: std_logic_vector(1 DOWNTO 0);
+
 BEGIN
- U0:codifica_boton PORT MAP(piso_donde_va=>piso_donde_va,codigo_piso=>codigo_piso);
-U1:FSM_ascensor PORT MAP(clk=>reloj,reset=>reset,codigo_piso=>codigo_piso,sube=>sube,baja=>baja,piso_donde_esta=>piso_donde_esta);
+	U0:codifica_boton PORT MAP(piso_donde_va=>piso_donde_va,codigo_piso=>codigo_piso);
+	U1:FSM_ascensor PORT MAP(clk=>reloj,reset=>reset,codigo_piso=>codigo_piso,sube=>sube,baja=>baja,piso_donde_esta=>piso_donde_esta);
 
 
 reloj <= NOT reloj AFTER periodo;
